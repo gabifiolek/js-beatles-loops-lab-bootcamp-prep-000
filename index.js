3cb124878eb9512569e826d085360b29b4fa420c
Gabriela Fijalkowska <gabrielafijalkowska@gmail.com>
2017-11-11 11:44:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var results = []
-
 function theBeatlesPlay(musicians, instruments) {
+  var results = [];
+  
   for (let i = 0; l = musicians.length; i < l; i++) {
     results[i] = musicians[i] + " plays " instruments[i];
     return results[i]
