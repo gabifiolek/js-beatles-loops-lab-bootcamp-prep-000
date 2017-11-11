73152d793410bf3301158365cea76f7e36592f08
Gabriela Fijalkowska <gabrielafijalkowska@gmail.com>
2017-11-11 14:43:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 function theBeatlesPlay(musicians, instruments) {
   var newArray = [];
+
   for (var i = 0; i < musicians.length; i++){
     newArray.push(musicians[i] + " plays " + instruments[i])
   }
+  
   return newArray;
 }
 
 function johnLennonFacts(facts) {
   var i = 0;
   var newFacts = [];
-  
+
   while (facts.length > i) {
     newFacts.push(facts[i] + "!!!");
     i++;
