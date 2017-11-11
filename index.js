feef6019319382faca0617538bc88592af03dca5
Gabriela Fijalkowska <gabrielafijalkowska@gmail.com>
2017-11-11 11:43:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var results = []
 function theBeatlesPlay(musicians, instruments) {
   for (let i = 0; l = musicians.length; i < l; i++) {
     results[i] = musicians[i] + " plays " instruments[i];
+    return results[i]
   }
-  return results
 }
 
 function johnLennonFacts(facts) {
