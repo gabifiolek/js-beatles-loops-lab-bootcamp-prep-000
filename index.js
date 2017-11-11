3bff59b098550cdc98d3e261cdf56ac806ef41ed
Gabriela Fijalkowska <gabrielafijalkowska@gmail.com>
2017-11-11 11:21:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function theBeatlesPlay(musicians, instruments) {
+
+  for (let i = 0; i < musicians.length; i++) {
+      var results[i] = musicians[i] + " plays " instruments[i];
+  }
+  return results
+}
+
+function johnLennonFacts(facts) {
+
+}
+
+function iLoveTheBeatles(number) {
+
+
+}
