9868ec04b68d2eb9b83ebafc865754f517a4065f
Gabriela Fijalkowska <gabrielafijalkowska@gmail.com>
2017-11-11 11:45:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 function theBeatlesPlay(musicians, instruments) {
   var results = [];
-  
+
   for (let i = 0; l = musicians.length; i < l; i++) {
-    results[i] = musicians[i] + " plays " instruments[i];
-    return results[i]
+    results.push(musicians[i] + " plays " + instruments[i]);
   }
 }
 
